5bd9e754a24f34c2ffa1196aa2901a67127b0ad1
Mirza Krak <mirza.krak@northern.tech>
2018-12-11 17:51:43 +0100

    convert-platform: use sudo when installing grub-env files
    Required in some cases.
    
    Signed-off-by: Mirza Krak <mirza.krak@northern.tech>

--- convert-platform.sh
+++ convert-platform.sh
@@ -111,7 +111,7 @@ generic() {
 
   make --quiet >> "$build_log" 2>&1
   log "\tInstalling GRUB2 boot scripts and tools."
-  make --quiet DESTDIR=${target_rootfs_dir} install >> "$build_log" 2>&1
+  sudo make --quiet DESTDIR=${target_rootfs_dir} install >> "$build_log" 2>&1
 }
 
 # Raspberry Pi specific boot firmware file
